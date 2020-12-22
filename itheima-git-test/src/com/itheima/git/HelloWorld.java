d3904c52c560048993c8e0a194e7d6aa564c22c4
kyle <huqingxunkyle@163.com>
2020-12-22 18:28:17 +0800

    12/22

--- itheima-git-test/src/com/itheima/git/HelloWorld.java
+++ itheima-git-test/src/com/itheima/git/HelloWorld.java
@@ -2,6 +2,8 @@ package com.itheima.git;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
+      System.out.println("hello world");
+      System.out.println("hello world");
+      System.out.println("hello world");
     }
 }
