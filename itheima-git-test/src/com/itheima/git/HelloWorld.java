b843928e2e9e75cba4c80e5d485c5454a753b3ff
kyle <huqingxunkyle@163.com>
2020-12-22 16:58:12 +0800

    第三次提交

--- itheima-git-test/src/com/itheima/git/HelloWorld.java
+++ itheima-git-test/src/com/itheima/git/HelloWorld.java
@@ -3,9 +3,6 @@ package com.itheima.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        System.out.println("Held");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
         System.out.println("HelloWorld");
     }
 }
